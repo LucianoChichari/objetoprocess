4096ef6a3ccf4b460f91547b786d4950c65baa51
LucianoChichari <lucianochichari@gmail.com>
2022-02-02 13:31:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const PORT = Number(argv._) || process.env.PORT
+const data = require('./processdata.js')
+const serverRoutes = require('./routes/index')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+app.set("views", "./views/layouts");
+
+app.get('/', (req, res) => {
+    res.send('server root')
+})
+
+app.get('/info', (req, res) => {
+    res.render('./views/layouts/index.ejs', {data: data})
+})
+
+serverRoutes(app)
+
+app.listen(PORT, () => console.log(`Servidor funcionando en http://localhost:${PORT}`))
+
